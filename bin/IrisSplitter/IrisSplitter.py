e0b83d7885ab986d6bbb66ed640a2dcd1eb61427
cheerupdi <dandonco3@gmail.com>
2018-06-19 14:47:50 +0900

    Add IrisSplitter module

--- bin/IrisSplitter/IrisSplitter.py
+++ bin/IrisSplitter/IrisSplitter.py
@@ -13,190 +13,187 @@ import Mobigen.Common.Log as Log
 G_SHUTDOWN = True
 
 def Shutdown(sigNum = 0, frame = 0) :
-        global G_SHUTDOWN
-        __LOG__.Trace("Recv Signal (%s)" % sigNum)
-        G_SHUTDOWN = False
+    global G_SHUTDOWN
+    __LOG__.Trace("Recv Signal (%s)" % sigNum)
+    G_SHUTDOWN = False
 
 signal.signal(signal.SIGTERM, Shutdown)  # Sig Terminate : 15
 signal.signal(signal.SIGINT, Shutdown)   # Sig Inturrupt : 2
 signal.signal(signal.SIGHUP, Shutdown)  # Sig HangUp : 1
 signal.signal(signal.SIGPIPE, Shutdown) # Sig Broken Pipe : 13
 
-class IrisSplitter() :
-        def __init__(self) :
+class IrisSplitter():
+    def __init__(self):
+        self.SECTION = sys.argv[1]
+        self.GetParser()
+        self.SetLog()
+        self.GetConfig()
 
-                self.SECTION = sys.argv[1]
+    def GetParser(self):
+        self.PARSER = ConfigParser.ConfigParser()
+        if os.path.exists(sys.argv[2]):
+            self.PARSER.read(sys.argv[2])
+        else:
+            print 'GetParser Error \n usage : %s ConfigFile' % (sys.argv[0])
+            sys.exit()
 
-                self.GetParser()
-                self.SetLog()
-                self.GetConfig()
+    def SetLog(self):
+        #default Log
+        self.logFilePath = 'Log'
+        self.logFileSize = 1000000
+        self.logFileCount = 10
 
-        def GetParser(self) :
+        if (self.PARSER != None):
+            if self.PARSER.get('Log','LogFilePath') != '':
+                self.logFilePath = self.PARSER.get('Log','LogFilePath')
+
+            self.logFileSize = self.PARSER.get('Log','LogFileSize')
+            self.logFileCount = self.PARSER.get('Log','LogFileCount')
+
+            if not os.path.exists(self.logFilePath):
+                os.makedirs(self.logFilePath)
+
+            LOG_NAME = '%s/%s_%s.log' % (self.logFilePath,
+                                         os.path.basename(sys.argv[0])[:-3],
+                                         self.SECTION)
+            Log.Init(Log.CRotatingLog(os.path.expanduser(LOG_NAME),
+                                      int(self.logFileSize),
+                                      int(self.logFileCount)))
+
+    def GetConfig(self):
+        try:
+            #Default 설정
+            self.SAVE_PATH = self.PARSER.get('COMMON', 'SAVE_PATH')
+            self.SAVE_SEPARATE = self.PARSER.get('COMMON', 'SAVE_SEPARATE')
+            self.FILE_SEPARATE = self.PARSER.get('COMMON', 'FILE_SEPARATE')
+            self.STDIN_STRING = self.PARSER.get('COMMON', 'STDIN_STRING')
+
+            #키 인덱스
+            self.KEY_INDEX = self.PARSER.getint('COMMON', 'KEY_INDEX')
+            if self.PARSER.has_option('COMMON', 'KEY_START_INDEX'):
+                ksidx = self.PARSER.getint('COMMON', 'KEY_START_INDEX')
+            if self.PARSER.has_option('COMMON', 'KEY_END_INDEX'):
+                keidx = self.PARSER.getint('COMMON', 'KEY_END_INDEX')
+
+            self.PARTITION_RANGE = self.PARSER.getint('COMMON', 'PARTITION_RANGE')
+            self.PARTITION_INDEX = self.PARSER.getint('COMMON', 'PARTITION_INDEX')
+
+            #개별 설정
+            if self.PARSER.has_option(self.SECTION, 'SAVE_PATH'):
+                self.SAVE_PATH = self.PARSER.get(self.SECTION, 'SAVE_PATH')
+            if self.PARSER.has_option(self.SECTION, 'SAVE_SEPARATE'):
+                self.SAVE_SEPARATE = self.PARSER.get(self.SECTION, 'SAVE_SEPARATE')
+            if self.PARSER.has_option(self.SECTION, 'FILE_SEPARATE'):
+                self.FILE_SEPARATE = self.PARSER.get(self.SECTION, 'FILE_SEPARATE')
+            if self.PARSER.has_option(self.SECTION, 'STDIN_STRING'):
+                self.STDIN_STRING = self.PARSER.get(self.SECTION, 'STDIN_STRING')
+
+            #키 인덱스
+            if self.PARSER.has_option(self.SECTION, 'KEY_INDEX'):
+                self.KEY_INDEX = self.PARSER.getint(self.SECTION, 'KEY_INDEX')
+            if self.PARSER.has_option(self.SECTION, 'KEY_START_INDEX'):
+                ksidx = self.PARSER.getint(self.SECTION, 'KEY_START_INDEX')
+            if self.PARSER.has_option(self.SECTION, 'KEY_END_INDEX'):
+                keidx = self.PARSER.getint(self.SECTION, 'KEY_END_INDEX')
+
+            self.KEY_SLICE = slice(ksidx, keidx)
+
+            if not os.path.exists(self.SAVE_PATH):
+                os.makedirs(self.SAVE_PATH)
+
+        except:
+            __LOG__.Exception()
+            sys.exit()
+
+    def run(self):
+        __LOG__.Trace('Start Make Iris Loader File!!')
+        while G_SHUTDOWN:
+            try:
+                # stdin read
+                stdin = sys.stdin.readline()
+                __LOG__.Trace('stdin : %s' % stdin)
                 try:
-                        self.PARSER = ConfigParser.ConfigParser()
-                        if os.path.exists(sys.argv[2]): self.PARSER.read(sys.argv[2])
-                        else: self.PARSER = None
+                    # check validity of stdin
+                    if stdin[:len(self.STDIN_STRING)] != self.STDIN_STRING:
+                        __LOG__.Trace('Error Stdin Data ==> %s' % stdin)
+                        sys.stderr.write(stdin)
+                        sys.stderr.flush()
+                        continue
                 except:
-                        print 'GetParser Error'
-                        self.PARSER = None
+                    __LOG__.Exception()
+                    sys.stderr.write(stdin)
+                    sys.stderr.flush()
+                    continue
 
-                if self.PARSER is None:
-                        print 'GetParser Error \n usage : %s ConfigFile' % ( sys.argv[0] )
-                        sys.exit()
+                FilePath = stdin[len(self.STDIN_STRING):].strip()
 
-        def SetLog(self) :
+                if not os.path.exists(FilePath):
+                    __LOG__.Trace('Error File Path Exists [%s]' % FilePath)
+                    sys.stderr.write(stdin)
+                    sys.stderr.flush()
+                    continue
 
-                #default Log
-                self.logFilePath = 'Log'
-                self.logFileSize = 1000000
-                self.logFileCount = 10
+                diFile = {}
+                with open(FilePath, 'r') as fd:
+                    for line in fd:
+                        row = line.strip().split(self.FILE_SEPARATE)
+                        Key = row[self.KEY_INDEX][self.KEY_SLICE]
+                        Partition = row[self.PARTITION_INDEX] #YYYYMMDDHHMMSS
 
-                if (self.PARSER != None) :
-                        if self.PARSER.get('Log','LogFilePath') != '' :
-                                self.logFilePath = self.PARSER.get('Log','LogFilePath')
+                        #Partition 설정
+                        Partition = Partition.translate(None,' -:/') #-:/ 제거함
 
-                        self.logFileSize = self.PARSER.get('Log','LogFileSize')
-                        self.logFileCount = self.PARSER.get('Log','LogFileCount')
+                        if self.PARTITION_RANGE == 1:
+                            Partition = Partition[:12] + '00'
+                        elif self.PARTITION_RANGE == 5:
+                            Partition[12] = str(int(Partition[12]) - \
+                                                int(Partition[12]) % 5)
+                        elif self.PARTITION_RANGE == 10:
+                            Partition = Partition[:11] + '000'
+                        elif self.PARTITION_RANGE == 60:
+                            Partition = Partition[:10] + '0000'
+                        elif self.PARTITION_RANGE == 1440:
+                            Partition = Partition[:8] + '000000'
 
-                        if not os.path.exists(self.logFilePath) :
-                                os.makedirs(self.logFilePath)
+                        #제외 인덱스? - 이건 추후에 적용
 
-                        LOG_NAME = '%s/%s_%s.log' % (self.logFilePath, os.path.basename(sys.argv[0])[:-3], self.SECTION)
-                        Log.Init(Log.CRotatingLog(os.path.expanduser(LOG_NAME), int(self.logFileSize), int(self.logFileCount)))
+                        FileName = '%s_%s_%s' % (Key,
+                                                 Partition,
+                                                 os.path.basename(FilePath))
 
+                        SaveFilePath = os.path.join(self.SAVE_PATH,FileName)
 
-        def GetConfig(self) :
-                try :
-                        #Default 설정
-                        self.SAVE_PATH = self.PARSER.get('COMMON', 'SAVE_PATH')
-                        self.SAVE_SEPARATE = self.PARSER.get('COMMON', 'SAVE_SEPARATE')
-                        self.FILE_SEPARATE = self.PARSER.get('COMMON', 'FILE_SEPARATE')
-                        self.STDIN_STRING = self.PARSER.get('COMMON', 'STDIN_STRING')
+                        row_w = self.SAVE_SEPARATE.join(str(v) for v in row)
+                        try:
+                            diFile[SaveFilePath].write(row_w + '\n')
+                        except:
+                            diFile[SaveFilePath] = open(SaveFilePath, 'w')
+                            diFile[SaveFilePath].write(row_w + '\n')
 
-                        #키 인덱스
-                        self.KEY_INDEX = self.PARSER.getint('COMMON', 'KEY_INDEX')
-                        try : ksidx = self.PARSER.getint('COMMON', 'KEY_START_INDEX')
-                        except : ksidx = None
-                        try : keidx = self.PARSER.getint('COMMON', 'KEY_END_INDEX')
-                        except : keidx = None
+                    for fp in diFile.keys():
+                        diFile[fp].close()
+                        __LOG__.Trace('file://%s\n' % diFile[fp].name)
+                        sys.stdout.write('file://%s\n' % diFile[fp].name)
+                        sys.stdout.flush()
+                        diFile[fp] = None
+                        del diFile[fp]
 
-                        self.PARTITION_RANGE = self.PARSER.getint('COMMON', 'PARTITION_RANGE')
-                        self.PARTITION_INDEX = self.PARSER.getint('COMMON', 'PARTITION_INDEX')
+                    diFile = {}
 
-                        #개별 설정
-                        try : self.SAVE_PATH = self.PARSER.get(self.SECTION, 'SAVE_PATH')
-                        except : pass
-                        try : self.SAVE_SEPARATE = self.PARSER.get(self.SECTION, 'SAVE_SEPARATE')
-                        except : pass
-                        try : self.FILE_SEPARATE = self.PARSER.get(self.SECTION, 'FILE_SEPARATE')
-                        except : pass
-                        try : self.STDIN_STRING = self.PARSER.get(self.SECTION, 'STDIN_STRING')
-                        except : pass
+                    sys.stderr.write('\n')
+                    sys.stderr.flush()
+            except :
+                __LOG__.Exception()
+                break
 
-                        #키 인덱스
-                        try : self.KEY_INDEX = self.PARSER.getint(self.SECTION, 'KEY_INDEX')
-                        except : pass
-                        try : ksidx = self.PARSER.getint(self.SECTION, 'KEY_START_INDEX')
-                        except : pass
-                        try : keidx = self.PARSER.getint(self.SECTION, 'KEY_END_INDEX')
-                        except : pass
+if __name__ == "__main__":
 
-                        self.KEY_SLICE = slice(ksidx,keidx)
+    if len(sys.argv) != 3:
+        print '%s Section ConfigPath' % sys.argv[0]
+        sys.exit()
 
-                        if not os.path.exists(self.SAVE_PATH) :
-                                os.makedirs(self.SAVE_PATH)
+    obj = IrisSplitter()
+    obj.run()
 
-                except :
-                        __LOG__.Exception()
-                        sys.exit()
+    __LOG__.Trace("PROCESS END...\n")
 
-        def run(self) :
-
-                __LOG__.Trace('Start Make Iris Loader File!!')
-
-                while G_SHUTDOWN :
-                        try :
-
-                                stdin = sys.stdin.readline()
-                                __LOG__.Trace('stdin : %s' % stdin)
-                                try :
-                                        if stdin[:len(self.STDIN_STRING)] != self.STDIN_STRING :
-                                                __LOG__.Trace('Error Stdin Data ==> %s' % stdin)
-                                                sys.stderr.write(stdin)
-                                                sys.stderr.flush()
-                                                continue
-                                except :
-                                        __LOG__.Exception()
-                                        sys.stderr.write(stdin)
-                                        sys.stderr.flush()
-                                        continue
-
-                                FilePath = stdin[len(self.STDIN_STRING):-1]
-
-                                if not os.path.exists(FilePath) :
-                                        __LOG__.Trace('Error File Path Exists [%s]' % FilePath)
-                                        sys.stderr.write(stdin)
-                                        sys.stderr.flush()
-                                        continue
-
-                                diFile = {}
-                                with open(FilePath, 'r') as fd :
-
-                                        for line in fd :
-
-                                                row = line[:-1].split(self.FILE_SEPARATE)
-
-                                                Key = row[self.KEY_INDEX][self.KEY_SLICE]
-                                                Partition = row[self.PARTITION_INDEX] #YYYYMMDDHHMMSS
-
-                                                #Partition 설정
-                                                Partition = Partition.translate(None,' -:/') #-:/ 제거함
-
-                                                if self.PARTITION_RANGE == 1 :
-                                                        Partition = Partition[:12] + '00'
-                                                elif self.PARTITION_RANGE == 5 :
-                                                        Partition[12] = str(int(Partition[12]) - int(Partition[12]) % 5)
-                                                elif self.PARTITION_RANGE == 10 :
-                                                        Partition = Partition[:11] + '000'
-                                                elif self.PARTITION_RANGE == 60 :
-                                                        Partition = Partition[:10] + '0000'
-                                                elif self.PARTITION_RANGE == 1440 :
-                                                        Partition = Partition[:8] + '000000'
-
-                                                #제외 인덱스? - 이건 추후에 적용
-
-                                                FileName = '%s_%s_%s' % (Key, Partition, os.path.basename(FilePath))
-                                                SaveFilePath = os.path.join(self.SAVE_PATH,FileName)
-
-                                                try :
-                                                        diFile[SaveFilePath].write(self.SAVE_SEPARATE.join(str(v) for v in row) + '\n')
-                                                except :
-                                                        diFile[SaveFilePath] = open(SaveFilePath, 'w')
-                                                        diFile[SaveFilePath].write(self.SAVE_SEPARATE.join(str(v) for v in row) + '\n')
-
-                                        for fp in diFile.keys() :
-                                                diFile[fp].close()
-                                                __LOG__.Trace('file://%s\n' % diFile[fp].name)
-                                                sys.stdout.write('file://%s\n' % diFile[fp].name)
-                                                sys.stdout.flush()
-                                                diFile[fp] = None
-                                                del diFile[fp]
-
-                                        diFile = {}
-
-                                        sys.stderr.write('\n')
-                                        sys.stderr.flush()
-                        except :
-                                __LOG__.Exception()
-                                break
-
-if __name__ == "__main__" :
-
-        if len(sys.argv) != 3 :
-                print '%s Section ConfigPath' % sys.argv[0]
-                sys.exit()
-
-        obj = IrisSplitter()
-        obj.run()
-
-        __LOG__.Trace("PROCESS END...\n")
---
+++ bin/IrisSplitter/README.md
@@ -0,0 +1,54 @@
+# IrisSplitter
+Stdin 으로 받은 file을 IRIS에 Loading하기 위해 Key와 Partition별로 분할저장합니다.
+서버의 시스템 시각 기준으로 저장하는 형식과, 로그파일의 사용자 시각 기준으로 저장하는 형식 두가지를 제공합니다.
+
+## How to use
+```Bash
+$ python <Section> <ConfigPath>
+```
+
+## STDIN
+```Bash
+<STDIN_STRING><FilePath>
+```
+예를 들어 Config파일의 STDIN_STRING이 file:// 로 설정 되어 있고 현재 디렉토리의 test.csv를 받는다면
+```Bash
+file://test.csv
+```
+
+## STDOUT
+```Bash
+file://<저장된 FilePath>
+```
+
+## Configuration
+
+**IrisSplitter.conf**
+|Section  |Option          |Range   |Description|
+|:-------:|:--------------:|--------|------------------------------------------------|
+|Log      |logfilepath     |(string)| log가 저장될 경로                              |
+|         |logfilesize     |(int)   | log파일의 최대 사이즈                          |
+|         |logfilecount    |(int)   | log파일의 최대 수                              |
+|COMMON   |SAVE_PATH       |(string)| 결과를 저장할 디렉토리 경로                    |
+|         |FILE_SEPARATE   |(string)| 읽는 파일의 열 구분자                          |
+|         |SAVE_SEPARATE   |(string)| 저장할 파일의 열 구분자                        |
+|         |STDIN_STRING    |(string)| STDIN의 Header 지정                            |
+|         |KEY_INDEX       |(int)   | Key 인덱스                                     |
+|         |KEY_START_INDEX |(int)   | Key 인덱스 슬라이스시 시작점                   |
+|         |KEY_END_INDEX   |(int)   | Key 인덱스 슬라이스시 끝점                     |
+|         |PARTITION_RANGE |(int)   | 여러개의 파일로 나눌 기준이 될 Partition Range |
+|         |PRATITION_INDEX |(int)   | Partition_Range를 적용할 Partition Index       |
+|<Section>|                || COMMON 섹션과 동일, 추가 적용할 옵션 덮어씌우기 용     |
+
+## Prerequisites
+- Python == 2.7
+- Python-M6-API == 1.1
+
+## How to unit test (Dynamic test)
+```Bash
+$ nosetests -v --with-coverage --with-doctest --cover-erase --exe  --cover-package=. tests/*.py
+```
+
+## How to lint (Static test)
+```Bash
+$ pylint --disable=C --disable=E0602 --msg-template='{path}:{line}:[{msg_id}({symbol}),{obj}]{msg}' *.py
