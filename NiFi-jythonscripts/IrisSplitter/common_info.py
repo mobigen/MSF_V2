2877d3de9fbcdca2e2ad5cbb4f93e635a160775f
root <yoon.cho@mobigen.com>
2019-08-01 18:00:00 +0900

    Add common config

---
+++ NiFi-jythonscripts/IrisSplitter/common_info.py
@@ -0,0 +1,19 @@
+iris_conn = {
+	"ip": "192.168.100.180",
+	"port": "5050",
+	"user": "cheerupdi",
+	"pwd": "cheerupdi.",
+	"db": "cheerupdi",
+	"timeout": 300,
+	"direct": False
+}
+
+log_info = {
+	"formatter": "%(asctime)s %(funcName)s:%(lineno)3d : %(message)s",
+	"log_dir": "/home/eva/user/sdi/log",
+	"mode": "a",
+	"maxBytes": 1000000,
+	"backupCount": 9
+}
+
+
